3e89dcf1108e85524fa090ecf3c92e43cf8aefb5
FlowerCake13 <lminami23@punahou.edu>
2017-01-28 09:57:24 -1000

    Update app.js

--- fake-binary-TFR/js/app.js
+++ fake-binary-TFR/js/app.js
@@ -1,14 +1,20 @@
-console.log("awio")
+//console.log("awio")
 
 function RoboTranslator(string) {
+	//console.log(string.length)
 	var i;
 	for (i = 0; i < string.length; i++){
-		if (string= typeof string[i]){
+		//console.log(string[i])
+		console.log(typeof string[i])
+		string[i] = "1";
+		console.log(string[i]);
+		/*if (typeof string[i] === "string"){
+			string[i] = "1";
+			console.log(string[i])
 			console.log("HEY IM HERE TOOOOO!")
-		}
-		console.log(string[i])
+		}*/
 	}
 }
 
-RoboTranslator("faioaefioae");
-RoboTranslator("Hello");
+RoboTranslator('h');
+//RoboTranslator("Hello");
